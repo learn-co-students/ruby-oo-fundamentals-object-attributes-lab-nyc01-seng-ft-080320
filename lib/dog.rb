473d5a3466403319d3cf4652b278619820e26b97
Vincent Collis <vinsanity0050@gmail.com>
2020-08-03 23:02:59 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+    # setters
+    def name=(dog_name)
+        @name = dog_name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+
+    # getters
+    def name
+        @name
+    end
+
+    def breed
+        @breed
+    end
+
+end
